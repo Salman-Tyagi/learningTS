4938ef37d10d69f234d6b11abdd6eaffcca755a4
Salman Tyagi <salmantyagi14@gmail.com>
2024-04-27 15:19:16 +0530

    write type annotation for some primitive and ojbect literal

---
+++ features/variables.ts
@@ -0,0 +1,31 @@
+const apples: number = 5;
+const speed: string = 'fast';
+
+let hasName: boolean = true;
+
+let nothing: null = null;
+let nothingMuch: undefined = undefined;
+
+
+// Build-in objects
+let time: Date = new Date();
+
+// Arrays
+let colors: string[] = ['red', 'blue', 'green'];
+
+let numbers: number[] = [1, 2, 4];
+
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+// Object literal
+let numObject: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
